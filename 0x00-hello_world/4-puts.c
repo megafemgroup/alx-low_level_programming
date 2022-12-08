37f14f4199c81765bcf87b2e592c43fbdd4d3f36
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-08 15:17:31 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* Description: ALX school
+* print using puts function
+* return: 0.
+*/
+int main(void)
+{
+	char word[] = "Programming is like building a multilingual puzzle";
+	puts(word);
+	return (0);
+}
