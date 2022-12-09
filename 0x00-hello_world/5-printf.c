12e6dc332e06875c73e133f665eab23283977581
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-08 16:02:04 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Description main - Print string using printf.
+ * Description: main - Print string using printf.
  * Return: 0 if success.
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
