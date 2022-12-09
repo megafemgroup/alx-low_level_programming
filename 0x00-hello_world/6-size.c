2800f629262f77fa5976bf9393fe9122472cc8f2
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-08 16:58:25 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a long: %lu bytes(s)\n", (unsigned long)sizeof(long));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n" (unsigned long)sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
