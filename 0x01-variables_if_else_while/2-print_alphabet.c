baaa2e5db14bcab6198774cd965ffbd99ee6402b
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-09 15:20:11 -0800

    alx

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
