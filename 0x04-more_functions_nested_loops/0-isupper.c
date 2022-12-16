ad7fece8b72a7f691e05050ecac77f433982ea11
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-16 13:53:30 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,6 +9,17 @@
  */
 
 int _isupper(int c)
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+int _isupper(int c)
 {
 
 	if (c >= 65 && c <= 90)
