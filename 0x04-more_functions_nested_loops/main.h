a0cc6615db9b5095d61d68e165f3cfdffd0b1bc3
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-16 13:39:05 -0800

    upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
-
+#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
