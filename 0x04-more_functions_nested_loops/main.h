065ffeacb9160634edca6b9e7560826bac8e8093
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-16 14:09:27 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 /*
  * File: main.h
  * Auth: Mehalayese Femi
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
-#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -22,4 +18,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
