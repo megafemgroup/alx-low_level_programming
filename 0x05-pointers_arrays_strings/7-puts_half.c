225aa141ede48d122cd4a253ec9a970835225e3c
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-19 18:32:02 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * puts_half - prints one char out of 2
+  * @str: input string
+  * Return: no return
+**/
+void puts_half(char *str)
+{
+int i = 0, n, m, j = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 != 0)
+{
+n = (i - 1) / 2;
+}
+else
+{
+n = i / 2;
+}
+m = i - n;
+while (j < n)
+{
+_putchar(str[j + m]);
+j++;
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,25 @@
 #include "main.h"
-
 /**
- * _strcpy - function with two arguments
- * @dest: 1st argument char type pointer
- * @src: 2nd argument char type pointer
- *
- * Description: copies the string to an array
- * Return: na
- */
+  * _strcpy - copies the string pointed to
+  * @dest: destination array
+  * @src: input string
+  * Return: string
+**/
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < src[i]; i++)
-		dest[i] = '\0';
-	return (dest);
+int i = 0, length = 0;
+while (src[i] != '\0')
+{
+i++;
+}
+length = i;
+for (i = 0; i < length && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for ( ; i <= length; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
