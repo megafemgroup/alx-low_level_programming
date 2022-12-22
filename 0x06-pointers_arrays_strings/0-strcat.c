93289155a8542d376ade8fea9ca5a6edf52ab9a9
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-22 11:27:36 -0800

    strconcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
+ * len - Function calculates the length of a given string.
+ * @dest: pointer to destination.
+ * Return: Always 0.
+ */
+int len(char *dest)
+{
+	return ((*dest != '\0') ? 1 + len(dest + 1) : 0);
+}
+/**
  * _strcat - function that copies a string.
  * @dest: This is pointer to the destination array.
  * @src: This is the string to be appended.
@@ -8,20 +17,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
-	while (dest[i] != '\0') 
+	char *d_itter = dest + len(dest);
+
+	while (*src != '\0')
 	{
-		dest[j] = dest[i];
-		i++;
-		j++;
+		*d_itter = *src;
+		src = src + 1;
+		d_itter = d_itter + 1;
 	}
-	i = 0;
-	while (src[i] != '\0') {
-		dest[j] = src[i];
-		i++;
-		j++;
-	}
-	dest[j] = '\0';
+	*d_itter = *src;
 	return (dest);
 }
