ed84fd0977f5282d018c4dd3725fd1824b2a5063
Mehalayese Femi <megafemgroup@gmail.com>
2022-12-22 09:46:45 -0800

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * Description: concate two string
+ * return : always return dest
+ */
+char *strcat(char *dest, char *src)
+{
+	int i;
+
+	int j = strlen(dest);
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i + j] = src[i];
+	}
+	dest[i + j] = '\0';
+	return(dest);
+}
