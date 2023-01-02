062e37f1de21bcbcb3684a4b65753189003ff104
Mehalayese Femi <megafemgroup@gmail.com>
2023-01-02 10:36:32 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,14 @@
  * Return: a pointer to the first occurance of the character @c in the string
  * @s. Return NULL if the character isn't found
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
+	int i;
 
+	for (i = 0; *(s + i) != '\0' ; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
 	return (NULL);
 }
