717102a558dfe8c6d1ac45a48d0ca066f05624fa
Femi Mehalayese <megafemgroup@gmail.com>
2023-01-06 16:39:23 -0800

    Tasks

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
- */
+* is_digit - validates if the character is a number
+* @str: the pointer to direct
+* Return: Result
+*/
+
+int is_digit(char *str)
+{
+	if (*str == '\0')
+	{
+		return (1);
+	}
+	return ((*str >= '0' && *str <= '9') && is_digit(str + 1));
+}
+
+/**
+* main - print product of multiplication of two arguments
+* @argc: counter of arguments
+* @argv: arguments
+* Return: (Success)
+*/
+
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		if (!atoi(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (!is_digit(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
 		}
-		sum += atoi(argv[i]);
+		printf("%d\n", sum);
+		return (0);
 	}
-	printf("%d\n", sum);
-
-	return (0);
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
 }
-
